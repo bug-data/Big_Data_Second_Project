0a9ee213b73f89a7856f129ec3b8533b8b1ee273
Jerin George Mathew <jerin.edassery95@gmail.com>
2019-07-15 13:14:38 +0200

    add script folder

---
+++ script/start-fake-spark-app.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo docker cp ../spark-app/spark-app.py spark-master:/spark-app.py
+sudo docker exec -it spark-master /spark/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0 spark-app.py
