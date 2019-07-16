3ac0c86fdb0051aff8f30f717d5422bfa9232e7f
Jerin George Mathew <jerin.edassery95@gmail.com>
2019-07-16 13:06:00 +0200

    add producer for node 8

---
+++ iot-data-producer/iot-node-producer.py
@@ -0,0 +1,52 @@
+import time
+import csv
+import json
+import sys
+from kafka import KafkaProducer
+
+NODE_ID = None
+BATCH_SIZE = 5
+FILENAME_FMT = 'pantheon20190612-nodo{}.csv'
+TOPIC_FMT = 'node{}'
+
+def create_producer(hostname, port):
+    producerEndpointFmt = '{}:{}'
+    return KafkaProducer(bootstrap_servers=producerEndpointFmt.format(hostname,port))
+
+def send_data(producerInstance, topicName, data):
+    producerInstance.send(topicName, data.encode('UTF-8'))
+    print('sent a message')
+    print(data)
+    print()
+    return
+    
+def send_batch_data(producerInstance, topicName, batch):
+    for data in batch:
+        send_data(producerInstance, topicName, data)
+    return
+
+def main():
+    
+    NODE_ID = sys.argv[1]
+    filename = FILENAME_FMT.format(NODE_ID)
+    topic = TOPIC_FMT.format(NODE_ID)
+    
+    producer = create_producer('kafka', '9092')
+    print(producer)
+    print()
+    
+    # read csv data from a single node
+    with open(FILENAME_FMT.format(NODE_ID)) as csvFile:
+        batch = []
+        reader = csv.DictReader(csvFile)
+        for row in reader:
+            batch.append(json.dumps(row))
+            if len(batch) == BATCH_SIZE:
+                send_batch_data(producer, topic, batch)
+                batch=[]
+                time.sleep(10) 
+
+    send_batch_data(producer, topic, batch)
+
+if __name__ == "__main__":
+    main()
--- script/start-producer.sh
+++ script/start-producer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo docker exec -it iot-source python dummy-producer.py
+sudo docker exec -it iot-source python iot-node-producer.py 8
